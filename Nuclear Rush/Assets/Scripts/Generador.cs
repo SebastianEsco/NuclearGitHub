a4bebc2b39fe62aa376cf7e578488e2d98133ab2
IssaMs46 <isams2004.1@gmail.com>
2024-04-02 09:55:10 -0500

    recoleccion de residuos y zona de transición (provisional)

--- Nuclear Rush/Assets/Scripts/Generador.cs
+++ Nuclear Rush/Assets/Scripts/Generador.cs
@@ -7,9 +7,13 @@ public class Generador : MonoBehaviour
     [SerializeField] private GameObject[] plataforma1 = new GameObject[4];
     [SerializeField] private GameObject[] plataforma2 = new GameObject[4];
     [SerializeField] private GameObject[] plataforma3 = new GameObject[4];
+    [SerializeField] private GameObject[] Transicion = new GameObject[1];
     [SerializeField] private int PuntoInicial, modulosPreTransformacion;
     int contadorDeModulosPasados;
     int aleatorio, numeroModulos, moduloActual;
+
+    public int numResiduo;
+
 
     void Start()
     {
@@ -20,7 +24,7 @@ public class Generador : MonoBehaviour
         if (other.gameObject.CompareTag("Trigger"))
         {
             contadorDeModulosPasados++;
-            Debug.Log("Colicion");
+            Debug.Log("Colision");
             Debug.Log(contadorDeModulosPasados);
 
             if(moduloActual == 0)
@@ -35,13 +39,35 @@ public class Generador : MonoBehaviour
             {
                 //Instantiate(plataforma3[aleatorio], new Vector3(PuntoInicial, 0, 0), Quaternion.identity);
             }
+            if (moduloActual == 2)
+            {
+                Instantiate(Transicion[1], new Vector3(PuntoInicial, 0, 0), Quaternion.identity);
+                Debug.Log("entra a transicion");
+            }
             
             aleatorio = Random.Range(0, numeroModulos);
             if(contadorDeModulosPasados == modulosPreTransformacion)
             {
                 contadorDeModulosPasados = 0;
                 moduloActual++;
+                if (moduloActual == 2)
+                {
+                    Debug.Log("entra a transicion");
+                }
+
+
             }
+        }
+        if (other.gameObject.CompareTag("Transicion"))
+        {
+            Debug.Log("cambiod e código");
+        }
+
+        if (other.gameObject.CompareTag("residuo"))
+        {
+            numResiduo++;
+            Debug.Log("Recoleccion residuo: " + numResiduo);
+            Destroy(other.gameObject);
         }
     }
 }
