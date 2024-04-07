a949ba959382c9d7d71f9df2247f2d643644d8d8
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-04-06 23:16:34 -0500

    Cambio en como se generan las plataformas
    Cambie la forma en que funcionan los scripts de las plataformas. Ahora tienen un punto en todo el principio como padre de todo y un punto al final que es el que genera las otras plataformas desde su posición.

--- Nuclear Rush/Assets/Scripts/Generador.cs
+++ Nuclear Rush/Assets/Scripts/Generador.cs
@@ -1,47 +1,39 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Generador : MonoBehaviour
 {
-    [SerializeField] private GameObject[] plataforma1 = new GameObject[4];
-    [SerializeField] private GameObject[] plataforma2 = new GameObject[4];
-    [SerializeField] private GameObject[] plataforma3 = new GameObject[4];
-    [SerializeField] private int PuntoInicial, modulosPreTransformacion;
-    int contadorDeModulosPasados;
-    int aleatorio, numeroModulos, moduloActual;
+    GestionadorDePlataformas gestionador;
+    private GameObject gestionadorObjeto;
+    GameObject plataforma;
 
-    void Start()
+    private bool instanciado;
+
+    public void Update()
     {
-        numeroModulos = plataforma1.Length;
+        
     }
-    public void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Trigger"))
-        {
-            contadorDeModulosPasados++;
-            Debug.Log("Colicion");
-            Debug.Log(contadorDeModulosPasados);
 
-            if(moduloActual == 0)
-            {
-                Instantiate(plataforma1[aleatorio], new Vector3(PuntoInicial, 0, 0), Quaternion.identity);
-            }
-            if (moduloActual == 1)
-            {
-                Instantiate(plataforma2[aleatorio], new Vector3(PuntoInicial, 0, 0), Quaternion.identity);
-            }
-            //if (moduloActual == 2)
-            {
-                //Instantiate(plataforma3[aleatorio], new Vector3(PuntoInicial, 0, 0), Quaternion.identity);
-            }
-            
-            aleatorio = Random.Range(0, numeroModulos);
-            if(contadorDeModulosPasados == modulosPreTransformacion)
-            {
-                contadorDeModulosPasados = 0;
-                moduloActual++;
-            }
+    public void Start()
+    {
+        gestionadorObjeto = GameObject.Find("ControladorPlataformas");
+        gestionador = gestionadorObjeto.GetComponent<GestionadorDePlataformas>();
+
+    }
+
+    public void FixedUpdate()
+    {
+        if(transform.position.x < 17 && !instanciado)
+        {
+            Debug.Log("Instanciando");
+            plataforma = gestionador.EscogerPlataforma();
+            Instantiate(plataforma, transform.position, Quaternion.identity);
+            instanciado = true;
         }
     }
+
+
+
 }
---
+++ Nuclear Rush/Assets/Scripts/GestionadorDePlataformas.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionadorDePlataformas : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] plataforma1 = new GameObject[4];
+    [SerializeField] private GameObject[] plataforma2 = new GameObject[4];
+    [SerializeField] private GameObject[] plataforma3 = new GameObject[4];
+    [SerializeField] private GameObject moduloDeTransformacion;
+    [SerializeField] private int modulosPreTransformacion;
+    int contadorDeModulosPasados;
+    int aleatorio, numeroModulos, moduloActual;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numeroModulos = plataforma1.Length;
+    }
+
+    public GameObject EscogerPlataforma()
+    {
+        
+        GameObject plataformaEscogida = null;
+
+        aleatorio = Random.Range(0, numeroModulos); //Escoger numero aleatorio
+
+        if (contadorDeModulosPasados == modulosPreTransformacion)
+        {
+            Debug.Log("MODULO DE TRANSFORMACION ACTIVADO, CAMBIANDO DE ZONA");
+            contadorDeModulosPasados = 0;
+            moduloActual++;
+            return moduloDeTransformacion;
+        }
+
+        if (moduloActual == 0)
+        {
+            plataformaEscogida = plataforma1[aleatorio];
+        }
+        else if (moduloActual == 1)
+        {
+            plataformaEscogida = plataforma2[aleatorio];
+        }
+        //if (moduloActual == 2)
+        {
+            //plataformaEscogida = plataforma3[aleatorio];
+        }
+
+        contadorDeModulosPasados++;
+
+        return plataformaEscogida;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Trigger"))
+        {
+            
+            Debug.Log("Colicion");
+            Debug.Log(contadorDeModulosPasados);
+
+            
+
+            
+        }
+
+
+    }
+}
