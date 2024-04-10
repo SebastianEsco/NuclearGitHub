7e053442493e315ac98d2d3c2af759320acbf85a
SebastianEsco <sebastian.escobarm@upb.edu.co>
2024-04-09 21:24:57 -0500

    Deslizamiento y colicion arreglada

--- Nuclear Rush/Assets/Scripts/PlayerController.cs
+++ Nuclear Rush/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
     bool antiDobleSalto;
-    Vector3 posicionInicial;
+    [SerializeField] Vector3 posicionInicial;
     Piso piso;
     Rigidbody rb;
     [SerializeField] float cantidadQueVaria, velocidadDeRecuperacion;
     public float fuerzaSalto;
     public bool puedeSaltar = true;
+
+    bool puedeDeslizarse;
+    [SerializeField] float tiempoDeDeslizamiento;
+    public GameObject hitBoxParado, hitBoxSentado;
+
     private void Start()
     {
         piso = GetComponent<Piso>();
         rb = GetComponent<Rigidbody>();
-        posicionInicial = transform.position;
         antiDobleSalto = true;
+        puedeDeslizarse = true;
+        hitBoxSentado.SetActive(false);
     }
 
     
@@ -25,15 +32,25 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        Vector3 posicion = transform.position;
-        
+        Acelerando();
 
+        Saltar();
+
+        RecuperarPosicionInicial();
+
+        Deslizarse();
+
+    }
+
+
+    public void Acelerando()
+    {
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
         {
             //Debug.Log("Presionando A o D");
-            if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
             {
-               // Debug.Log("Presionando A y D");
+                // Debug.Log("Presionando A y D");
                 piso.velocidad = piso.velocidadInicial;
             }
             else if (Input.GetKey(KeyCode.D))
@@ -51,7 +68,10 @@ public class PlayerController : MonoBehaviour
         {
             piso.velocidad = piso.velocidadInicial;
         }
+    }
 
+    public void Saltar()
+    {
         //Castear Rasho laser
         RaycastHit hit;
         if (Physics.Raycast(transform.position, -Vector3.up, out hit, 0.75f, 1) && antiDobleSalto) //Tirar el rayo para abajo
@@ -60,17 +80,11 @@ public class PlayerController : MonoBehaviour
             Invoke("AntiDobleSalto", 0.1f);
             puedeSaltar = true; //Si le está pegando al suelo, puede saltar
         }
-        
+
         if (Input.GetKeyDown(KeyCode.W) && puedeSaltar) //Apretar W y estar en el suelo
         {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode.Impulse);
             puedeSaltar = false;
-        }
-
-        //Recuperar posiciòn Inicial de a poquito
-        if(transform.position.x < posicionInicial.x)
-        {
-            transform.position = new Vector3(posicion.x + (velocidadDeRecuperacion * Time.deltaTime), posicion.y, posicion.z);
         }
     }
 
@@ -78,6 +92,44 @@ public class PlayerController : MonoBehaviour
     {
         antiDobleSalto = true;
     }
-    
+
+    public void RecuperarPosicionInicial()
+    {
+
+        Vector3 posicion = transform.position;
+        //Recuperar posición Inicial de a poquito
+        if (transform.position.x < posicionInicial.x)
+        {
+            transform.position = new Vector3(posicion.x + (velocidadDeRecuperacion * Time.deltaTime), posicion.y, posicion.z);
+        }
+    }
+
+    public void Deslizarse()
+    {
+        if(puedeSaltar && puedeDeslizarse && Input.GetKeyDown(KeyCode.S)) //Revisar que no esté en el aire ni deslizandose
+        {
+            CambiarHitBox(); //Cambiar a sentado
+            Invoke("CambiarHitBox", tiempoDeDeslizamiento);
+            puedeDeslizarse = false;
+            
+        }
+    }
+
+    public void CambiarHitBox()
+    {
+        if (hitBoxParado.activeSelf)
+        {
+            hitBoxSentado.SetActive(true); //Se sienta
+            hitBoxParado.SetActive(false);
+        }
+        else
+        {
+            hitBoxSentado.SetActive(false); //Se para
+            hitBoxParado.SetActive(true);
+            puedeDeslizarse = true;
+        }
+
+    }
+
 
 }
